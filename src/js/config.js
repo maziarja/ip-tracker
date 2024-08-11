250f0c524415774f21b0c5799b1ec9161d5759cc
maziarja <maziar.ja@gmail.com>
2024-08-11 12:42:00 -0700

    config-hidden

--- src/js/config.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = `apiKey=at_8tqjfPzWI9c7bclHOhzfMOE61Fx6D`;
-export const API_URL = `https://geo.ipify.org/api/v2/country,city`;
-export const TIME_OUT_SEC = 5;
-export const MAP_ZOOM = 16;
